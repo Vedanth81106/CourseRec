bfba26318817b0d83f9ac33adf432dde6d8159cc
kermit504 <ashiya.jendi@gmail.com>
2026-07-26 21:53:24 +0530

    Finished crud end points

--- app/routers/students.py
+++ app/routers/students.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from app.database import get_db
 from app.schemas.student import StudentCreate, StudentResponse
-from app.services.student_service import create_student
+from app.services.student_service import *
 
 router = APIRouter(
     prefix="/students",
@@ -11,9 +11,22 @@ router = APIRouter(
 )
 
 
-@router.get("/")
-def get_students():
-    return {"message": "Student route works"}
+@router.get("/", response_model=list[StudentResponse])
+def get_students(db: Session = Depends(get_db)):
+    return get_all_students(db)
+
+
+@router.get("/{student_id}", response_model=StudentResponse)
+def get_student(student_id: int, db: Session = Depends(get_db)):
+    student = get_student_by_id(db, student_id)
+
+    if student is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Student not found"
+        )
+
+    return student
 
 
 @router.post("/", response_model=StudentResponse)
@@ -22,3 +35,38 @@ def add_student(
     db: Session = Depends(get_db)
 ):
     return create_student(db, student)
+
+
+@router.put("/{student_id}", response_model=StudentResponse)
+def edit_student(
+    student_id: int,
+    student: StudentCreate,
+    db: Session = Depends(get_db)
+):
+
+    updated = update_student(db, student_id, student)
+
+    if updated is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Student not found"
+        )
+
+    return updated
+
+
+@router.delete("/{student_id}")
+def remove_student(
+    student_id: int,
+    db: Session = Depends(get_db)
+):
+
+    deleted = delete_student(db, student_id)
+
+    if deleted is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Student not found"
+        )
+
+    return {"message": "Student deleted successfully"}
--- app/services/student_service.py
+++ app/services/student_service.py
@@ -17,7 +17,61 @@ def create_student(db: Session, student: StudentCreate):
         learning_style=student.learning_style,
         career_goal=student.career_goal
     )
+
     db.add(db_student)
     db.commit()
     db.refresh(db_student)
+
     return db_student
+
+
+def get_all_students(db: Session):
+    return db.query(Student).all()
+
+
+def get_student_by_id(db: Session, student_id: int):
+    return (
+        db.query(Student)
+        .filter(Student.student_id == student_id)
+        .first()
+    )
+
+def update_student(db: Session, student_id: int, student_data: StudentCreate):
+
+    student = db.query(Student).filter(
+        Student.student_id == student_id
+    ).first()
+
+    if student is None:
+        return None
+
+    student.name = student_data.name
+    student.email = student_data.email
+    student.password = student_data.password
+    student.degree = student_data.degree
+    student.branch = student_data.branch
+    student.semester = student_data.semester
+    student.cgpa = student_data.cgpa
+    student.interests = student_data.interests
+    student.learning_style = student_data.learning_style
+    student.career_goal = student_data.career_goal
+
+    db.commit()
+    db.refresh(student)
+
+    return student
+
+
+def delete_student(db: Session, student_id: int):
+
+    student = db.query(Student).filter(
+        Student.student_id == student_id
+    ).first()
+
+    if student is None:
+        return None
+
+    db.delete(student)
+    db.commit()
+
+    return student
