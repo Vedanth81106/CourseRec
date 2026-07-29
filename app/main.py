8a78a5722ee87994d89c47d927012c516a1fb059
Vedanth <vedanth81106@gmail.com>
2026-07-29 22:22:51 +0530

    Added docker files

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from app.routers.students import router as student_router
 from app.routers.course import router as course_router
 from app.routers.enrollment import router as enrollment_router
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from app.database import Base, engine
 # import all the database models
@@ -14,6 +15,15 @@ app.include_router(student_router)
 app.include_router(course_router)
 app.include_router(enrollment_router)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
 def home():
     return {"message": "CourseRec API is running"}
